3b3fed81198483862f5bab2357f3db5643906deb
AnnyaB <anabordin2008@gmail.com>
2024-05-05 16:13:24 -0300

    finalizacao

---
+++ IdadeAnoBissexto/Pessoa.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdadeAnoBissexto
+{
+    internal class Pessoa
+    {
+        string Nome;
+        int Idade;
+        string Profissao;
+
+        public Pessoa()
+        {
+        }
+
+        public void SetNome(string nome)
+        {
+            Nome = nome;
+        }
+        public void SetIdade(int idade)
+        {
+            Idade = idade;
+        }        
+        public void SetProfissao(string profissao)
+        {
+            Profissao = profissao;
+        }
+        public string GetNome()
+        {
+            return Nome;
+        }
+        public int GetIdade()
+        {
+            return Idade;
+        }
+        public string GetProfissao()
+        {
+            return Profissao;
+        }
+        public int CalcularIdadeBissexto(int idade, int anoAtual)
+        {
+            int anosBissexto = 0;
+            for (int i = 0; i < idade; i++)
+            {
+                if (DateTime.IsLeapYear(anoAtual - i))
+                    anosBissexto++;
+            }
+            return anosBissexto;
+        }
+    }
+}
--- IdadeAnoBissexto/Program.cs
+++ IdadeAnoBissexto/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace IdadeAnoBissexto
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace IdadeAnoBissexto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pessoa pessoa = new Pessoa();
+            Console.WriteLine("Digite o nome da pessoa:");
+            pessoa.SetNome(Console.ReadLine());
+            
+            Console.WriteLine("Digite a idade da pessoa:");
+            int idade = 0; 
+            do
+            {
+                idade = int.Parse(Console.ReadLine());
+                if (idade < 0 || idade > 120)
+                    Console.WriteLine("Digite uma idade válida!");
+            } while (idade < 0 || idade > 120);
+            
+            pessoa.SetIdade(idade);
+            
+            Console.WriteLine("Digite a profissão da pessoa:");
+            pessoa.SetProfissao(Console.ReadLine());
+
+            int anoAtual = DateOnly.FromDateTime(DateTime.Now).Year;
+            int anosBissexto = pessoa.CalcularIdadeBissexto(idade, anoAtual);
+            Console.WriteLine($"{pessoa.GetNome()}, tem {pessoa.GetIdade()} e é {pessoa.GetProfissao()}, {pessoa.GetNome()} teria {anosBissexto} anos se só fizesse aniversário nos anos bissextos!");
         }
     }
 }
